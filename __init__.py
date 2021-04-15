a1ae7f5ce3fa07bf682a7f08ee45a02fe4efee6b
Simon Waller <waller_as@yahoo.co.uk>
2021-04-15 15:52:27 +0100

    first attempt

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_handler
+import requests
+from mycroft.util.log import getLogger
 
+LOGGER = getLogger(__name__)
 
 class ShellyLights(MycroftSkill):
     def __init__(self):
@@ -7,7 +10,17 @@ class ShellyLights(MycroftSkill):
 
     @intent_file_handler('lights.shelly.intent')
     def handle_lights_shelly(self, message):
-        self.speak_dialog('lights.shelly')
+        light{'name'} = = message.data.get('name')
+        light{'state'} = = message.data.get('state')
+        LOGGER.info(f"Trying to turn {light{'name'}} {light{'state'}}")
+        if (names{light{'name'}} is None):
+            self.speak_dialog('unknown', data={'name': light{'name'}}, wait=False)
+            return
+        res = requests.get('http://' + ip{light{'name'}} + '/relay/0?turn=' + light{'state'})
+        if (res.status_code == 200):
+            self.speak_dialog('lights.shelly', data={'name': light{'name'}, 'state': light{'state'}}, wait=False)
+        else:
+            self.speak_dialog('fail', data={'name': light{'name'}, 'state': light{'state'}}, wait=False)
 
 
 def create_skill():
